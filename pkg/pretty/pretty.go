c88893e54b72288fccda2879b1de68a707455dfd
Jonas Fan <jianfan.tw@gmail.com>
2021-02-28 01:04:49 +0800

    pretty: use replace instead

--- pkg/pretty/pretty.go
+++ pkg/pretty/pretty.go
@@ -7,12 +7,10 @@ import (
 // IndentWith adds specified spaces in front of each line of message
 func IndentWith(message string, spaces string) string {
 	if message == "" {
-		return ""
+		return message
 	}
 
-	lines := strings.Split(message, "\n")
-
-	return spaces + strings.Join(lines, "\n"+spaces)
+	return spaces + strings.ReplaceAll(message, "\n", "\n"+spaces)
 }
 
 // Indent adds 4 spaces in front of each line of message
