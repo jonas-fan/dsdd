df5ea7e78a1af524cb7d32e01f081dace3fe9103
Jonas Fan <jianfan.tw@gmail.com>
2021-04-01 22:40:02 +0800

    logs: improve performance

--- internal/cmd/logs/logs.go
+++ internal/cmd/logs/logs.go
@@ -1,12 +1,45 @@
 package logs
 
 import (
-	"github.com/jonas-fan/dsdd/pkg/dsa/diagnostic"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
+func pipeAll(writer io.Writer, filenames []string) {
+	for _, each := range filenames {
+		file, err := os.Open(each)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if _, err := io.Copy(writer, file); err != nil {
+			panic(err)
+		}
+
+		file.Close()
+	}
+}
+
 func run(cmd *cobra.Command, args []string) {
-	diagnostic.ReadLog()
+	pattern := filepath.Join("Agent", "logs", "ds_agent*.log")
+	matches, err := filepath.Glob(pattern)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(matches); i++ {
+		if strings.Contains(matches[i], "err") {
+			matches = append(matches[:i], matches[i+1:]...)
+		}
+	}
+
+	pipeAll(os.Stdout, matches)
 }
 
 func NewCommand() *cobra.Command {
